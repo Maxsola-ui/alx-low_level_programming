2620108e2eff7b35858103efc00b099e9a0d1207
Maxsola-ui <Fakinbule@gmail.com>
2022-12-09 08:35:14 -0800

    Correction of task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,16 +16,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	printf("last digit of %d is ", n);
 	num = n % 10;
-
-	if (n > 5)
+	if (num > 5)
 	{
 		printf("%d and is greater than 5\n", num);
 	}
 	else if ((num < 6) && (num < 0))
 	{
-		printf("%d ans is less than 6 and not 0\n", num);
+		printf("%d and is less than 6 and not 0\n", num);
 	}
-	else
+	else if (num == 0)
 	{
 		printf("%d and is 0\n", num);
 	}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,2 @@
 Script for C variables on if, else and while
+This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print whether the number stored in the variable n is positive or negative. The variable n will store a different balue every time you will run this program
