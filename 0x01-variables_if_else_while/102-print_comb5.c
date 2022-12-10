98e414c60bcca317a485b9e7a4f5644f5b4135ef
Maxsola-ui <Fakinbule@gmail.com>
2022-12-09 17:18:49 -0800

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,24 +10,26 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 99; i++)
+	for (i = 0; i < 100; i++)
 	{
-		for (j = i + 1; j < 99; j++)
+		for (j = 0; j < 100; j++)
 		{
-			putchar('0' + i / 10);
-			putchar('0' + i % 10);
-
-			putchar(' ');
-
-			putchar('0' + j / 10);
-			putchar('0' + j % 10);
-
-			if (i == 98 && j == 99)
-				break;
-			putchar(',');
-			putchar(' ');
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
-	putchar('\n');	
+	putchar('\n');
+
 	return (0);
 }
