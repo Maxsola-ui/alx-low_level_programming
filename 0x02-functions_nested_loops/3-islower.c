496f1beb6f738aac9769cd2da9205d0a1dfe1790
Maxsola-ui <Fakinbule@gmail.com>
2022-12-13 14:24:51 -0800

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main- check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int n, c;
+
+	c = 0;
+	while (c <= 1)
+	{
+		for (n = 'a'; n <= c; n++)
+		{
+			_putchar(n);
+		}
+
+		c++;
+		else
+		{
+			_putchar("0\n");
+		}
+	}
+}
+
