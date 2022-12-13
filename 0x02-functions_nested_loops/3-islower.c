61d34af2faf64a1b581970b3596be61c78095511
Maxsola-ui <Fakinbule@gmail.com>
2022-12-13 15:00:54 -0800

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,12 +8,14 @@
 
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-	{
-		_putchar('1');
-		return (1);
-	}
-	_putchar('0');
+	int r;
 
+	r = _islower('H');
+	_putchar(r + '0');
+	r = _islower('o');
+	_putchar(r + '0');
+	r = _islower(108);
+	_putchar(r + '0');
+	_putchar('\n');
 	return (0);
 }
