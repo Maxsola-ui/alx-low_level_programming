e5ebf892dd921c4df000a0a44b2243fbd64c7fc6
Maxsola-ui <Fakinbule@gmail.com>
2022-12-13 16:26:40 -0800

    Task 4 to 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main"
+
+/**
+ * print_last_digit - prints the last digit
+ * @n: The number
+ * Return: Value of the last digit
+ */
+
+int print_last_digit(int n)
+{
+	int ld = n % 10;
+
+	if (ld < 0)
+		ld *= -1;
+
+	_putchar(ld + '0');
+
+	return (0);
+}
