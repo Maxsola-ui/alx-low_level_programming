b8aa3eb8df07671a35a14ef933664dae40f5b213
Maxsola-ui <Fakinbule@gmail.com>
2022-12-14 14:35:27 -0800

    Task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
 
 /**
- * 103-fibonacci - Generate fibonacci sequence  by adding the previous 2 terms
+ * main - Generate fibonacci sequence  by adding the previous 2 terms
  * Return: Always 0 if no error
  */
 
 int main(void)
 {
-	int a = 1, b = 2;
+	int a = 0, b = 1, next = 0;
 	int sum = 0;
 
 	while (b <= 4000000)
 	{
-		if (b % 2 == 0)
-		{
-			sum += b;
-		}
-		int c = a + b;
-
+		next = a + b;
 		a = b;
-		b = c;
+		b = next;
+		if (next % 2 == 0)
+			sum += next;
 	}
-	printf("%d\n", sum);
+	printf("%i\n", sum);
 	return (0);
 }
