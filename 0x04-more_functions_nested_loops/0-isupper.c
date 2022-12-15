33218badd8428c772207f6909b5e57b8d3af5fcc
Maxsola-ui <Fakinbule@gmail.com>
2022-12-15 12:13:24 -0800

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,12 @@
 #include "main.h"
 
 /**
- * _isupper - checks uppercase
- * @c: parameter
- * Return: 0 if successful
+ * _isupper - function that checks uppercase alphas
+ * @c: an integer
+ * Return: something but success
  */
 
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	return (c >= 65 && c <= 90);
 }
