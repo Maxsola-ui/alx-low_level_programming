0747947e7d127bce2d5e214f7d6048581cd4228b
Maxsola-ui <Fakinbule@gmail.com>
2022-12-15 17:30:14 -0800

    print triabgle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,16 +8,23 @@
 
 void print_triangle(int size)
 {
-	int i, j;
-
-	for (i = 0; i < size; i++)
+	int i, j, n = size -1;
+	 
+	if (size > 0)
 	{
-		for (j = 1; j < (size - i); j++)
-			_putchar(' ');
-		for (j--; j < size; j++)
-			_putchar(35);
-		if (i < (size - 1))
-			_putchar('\n');
-	}
-	_putchar('\n');
-}
+		for (; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				if (j < n)
+					_putchar(' ');
+				else
+					_putchar('#'_;
+							}
+							n--;
+							_putchar('\n');
+							}
+							}
+							else
+							_putchar('\n');
+							}
