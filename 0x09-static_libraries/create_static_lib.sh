ebe5bdcd6d14fb337cd7a380c8593060e01b682b
Maxsola-ui <Fakinbule@gmail.com>
2023-01-06 08:28:52 -0800

    Script for libraries

---
+++ 0x09-static_libraries/create_static_lib.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Create an empty array to store the names of the .c files
+c_files=()
+
+# Iterate through all the .c files in the current directory
+
+for file in ./*.c
+do
+	# Add the name of the file to the array
+	c_files+=($file)
+done
+
+# Create the static library with the .c files
+ar rcs liball.a ${c_files[@]}
